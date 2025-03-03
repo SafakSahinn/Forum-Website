653a31aeedc4190b9bde2bfc1a5a44fdcef27883
safak <safaksahin022@gmail.com>
2025-03-03 12:30:39 +0300

    44

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete;
+using FluentValidation;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı boş geçilemez!");
+            RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Yazar soyadı boş geçilemez!");
+            RuleFor(x => x.WrıterAbout).NotEmpty().WithMessage("Hakkında kısmı boş geçilemez!");
+
+            RuleFor(x => x.WriterName).MinimumLength(3).WithMessage("Yazar adı en az 3 karakterden oluşmalıdır!");
+            RuleFor(x => x.WriterSurname).MinimumLength(3).WithMessage("Yazar soyadı en az 3 karakterden oluşmalıdır!");
+            RuleFor(x => x.WrıterAbout).MinimumLength(3).WithMessage("Hakkında kısmı en az 3 karakterden oluşmalıdır!");
+
+            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Yazar adı 50 karakterden uzun olamaz!");
+            RuleFor(x => x.WriterSurname).MaximumLength(50).WithMessage("Yazar soyadı 50 karakterden uzun olamaz!");
+            RuleFor(x => x.WrıterAbout).MaximumLength(100).WithMessage("Hakkında kısmı 100 karakterden uzun olamaz!");
+        }
+    }
+}
--- MvcProjeKampi/Controllers/WriterController.cs
+++ MvcProjeKampi/Controllers/WriterController.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 
 namespace MvcProjeKampi.Controllers
 {
@@ -16,6 +19,32 @@ namespace MvcProjeKampi.Controllers
         {
             var WriterValues = wm.GetList();
             return View(WriterValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddWriter()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddWriter(Writer p)
+        {
+            WriterValidator writervalidator = new WriterValidator();
+            ValidationResult results = writervalidator.Validate(p);
+            if (results.IsValid)
+            {
+                wm.WriterAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
