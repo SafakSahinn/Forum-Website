62e9b97fefc1b3b4ab244a7fb5488c200b7e18a3
safak <safaksahin022@gmail.com>
2025-03-03 13:14:51 +0300

    45

--- BusinessLayer/ValidationRules/WriterValidator.cs
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -15,14 +15,17 @@ namespace BusinessLayer.ValidationRules
             RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adı boş geçilemez!");
             RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Yazar soyadı boş geçilemez!");
             RuleFor(x => x.WrıterAbout).NotEmpty().WithMessage("Hakkında kısmı boş geçilemez!");
+            RuleFor(x => x.WriterTitle).NotEmpty().WithMessage("Ünvan kısmı boş geçilemez!");
 
             RuleFor(x => x.WriterName).MinimumLength(3).WithMessage("Yazar adı en az 3 karakterden oluşmalıdır!");
             RuleFor(x => x.WriterSurname).MinimumLength(3).WithMessage("Yazar soyadı en az 3 karakterden oluşmalıdır!");
             RuleFor(x => x.WrıterAbout).MinimumLength(3).WithMessage("Hakkında kısmı en az 3 karakterden oluşmalıdır!");
+            RuleFor(x => x.WriterTitle).MinimumLength(3).WithMessage("Ünvan kısmı en az 3 karakterden oluşmalıdır!");
 
             RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Yazar adı 50 karakterden uzun olamaz!");
             RuleFor(x => x.WriterSurname).MaximumLength(50).WithMessage("Yazar soyadı 50 karakterden uzun olamaz!");
             RuleFor(x => x.WrıterAbout).MaximumLength(100).WithMessage("Hakkında kısmı 100 karakterden uzun olamaz!");
+            RuleFor(x => x.WriterTitle).MaximumLength(50).WithMessage("Ünvan kısmı 100 karakterden uzun olamaz!");
         }
     }
 }
